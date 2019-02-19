d4f282f05c97ac7de2d431a4b95c6808be8d56d0
CareyFrancis <carexfm@gmail.com>
2019-02-19 08:33:15 +0300

    Populate the config.py file

--- config.py
+++ config.py
@@ -1,15 +1,42 @@
+import os
 
 class Config:
-    pass
+    """
+    This is the class which will contain the general configurations
+    """
+    SECRET_KEY = os.environ.get("carexfm")
+    SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://carey:carexfm.@localhost/Blog' 
+    MAIL_SERVER = "smtp.gmail.com"
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    UPLOADED_PHOTOS_DEST = "app/static/photos"
 
-class ProdConfig(Config):
-    pass
-
-class TestConfig(Config):
-    pass
 
 class DevConfig(Config):
+    """
+    This is the class which will contain the development configurations
+    """
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://carey:carexfm@localhost/Blog'
     DEBUG = True
 
-config_options ={"production":ProdConfig,"default":DevConfig,"testing":TestConfig}
+
+class ProdConfig(Config):
+    """
+    This is the class which will contain the production configurations
+    """
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+
+class TestConfig(Config):
+    """
+    This is the class which will contain the test configurations
+    """
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://carey:carexfm@localhost/Blog'
+
+config_options = {
+    "development": DevConfig,
+    "test": TestConfig,
+    "production": ProdConfig
+}
 
